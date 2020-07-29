ed198bb02b497371047fc2f358c68ebb6340fecd
Nawras GEORGI <admin@nawrasg.fr>
2020-07-29 21:31:02 +0200

    Add SPM

--- Package.swift
+++ Package.swift
@@ -17,7 +17,8 @@ let package = Package(
     targets: [
         .target(
             name: "CollapsibleTableSectionViewController",
-            dependencies: []),
+            dependencies: [],
+            path: "./"),
         .testTarget(
             name: "CollapsibleTableSectionViewControllerTests",
             dependencies: ["CollapsibleTableSectionViewController"]),
